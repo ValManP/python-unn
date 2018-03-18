f2de9fdfb9c75d4b2f9df084dfb8521e28f044e6
Valery Pozdyaev <ValPozd@mail.ru>
2018-03-18 18:38:52 +0300

    Fix mistype coefs to coeffs

--- Polynomial.py
+++ Polynomial.py
@@ -3,24 +3,24 @@ class Polynomial:
     # input: a_n, .. , a_1, a_0 or other polynomial
     def __init__(self, *args):
         if len(args) == 1 and isinstance(args[0], Polynomial):
-            self.coefs = args[0].coefs[:]
+            self.coeffs = args[0].coeffs[:]
         else:
-            self.coefs = args[::-1]
+            self.coeffs = args[::-1]
         self.trim()
 
     def __add__(self, other):
         res = []
 
         if isinstance(other, Polynomial):
-            for i in range(max(len(self.coefs), len(other.coefs))):
-                if i >= len(self.coefs):
-                    res.append(other.coefs[i])
-                elif i >= len(other.coefs):
-                    res.append(self.coefs[i])
+            for i in range(max(len(self.coeffs), len(other.coeffs))):
+                if i >= len(self.coeffs):
+                    res.append(other.coeffs[i])
+                elif i >= len(other.coeffs):
+                    res.append(self.coeffs[i])
                 else:
-                    res.append(self.coefs[i] + other.coefs[i])
+                    res.append(self.coeffs[i] + other.coeffs[i])
         else:
-            res = [t + other for t in list(self.coefs)]
+            res = [t + other for t in list(self.coeffs)]
 
         res = res[::-1]
         return Polynomial(*res)
@@ -30,29 +30,29 @@ class Polynomial:
 
     def __mul__(self, other):
         if isinstance(other, Polynomial):
-            s = list(self.coefs)
-            o = list(other.coefs)
+            s = list(self.coeffs)
+            o = list(other.coeffs)
 
             res = [0] * (len(s) + len(o) - 1)
             for self_pow, self_coeff in enumerate(s):
                 for other_pow, other_coeff in enumerate(o):
                     res[self_pow + other_pow] += self_coeff * other_coeff
         else:
-            res = [t * other for t in list(self.coefs)]
+            res = [t * other for t in list(self.coeffs)]
 
         res = res[::-1]
         return Polynomial(*res)
 
     def __eq__(self, other):
-        return self.coefs == other.coefs
+        return self.coeffs == other.coeffs
 
     def __ne__(self, other):
-        return self.coefs != other.coefs
+        return self.coeffs != other.coeffs
 
     def __str__(self):
         res = []
 
-        for self_pow, self_coeff in enumerate(self.coefs):
+        for self_pow, self_coeff in enumerate(self.coeffs):
             if self_coeff:
                 if self_pow == 0:
                     self_pow = ''
@@ -77,12 +77,12 @@ class Polynomial:
             return '0'
 
     def trim(self):
-        coefs = list(self.coefs)
-        if coefs:
-            max_not_empty = len(coefs) - 1
-            if coefs[max_not_empty] == 0:
+        coeffs = list(self.coeffs)
+        if coeffs:
+            max_not_empty = len(coeffs) - 1
+            if coeffs[max_not_empty] == 0:
                 max_not_empty -= 1
-                while max_not_empty >= 0 and coefs[max_not_empty] == 0:
+                while max_not_empty >= 0 and coeffs[max_not_empty] == 0:
                     max_not_empty -= 1
-                del coefs[max_not_empty + 1:]
-        self.coefs = tuple(coefs)
+                del coeffs[max_not_empty + 1:]
+        self.coeffs = tuple(coeffs)
