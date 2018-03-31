d21fafbe61d43002c7f5260a56c6035f3f74a595
ValManP <valpozd@mail.ru>
2018-03-31 13:26:46 +0300

    fix codecoc

--- polynomial_test.py
+++ polynomial_test.py
@@ -190,3 +190,7 @@ class PolynomialTest(unittest.TestCase):
         # Assert
         expected = Polynomial(0)
         self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
